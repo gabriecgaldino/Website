45000647bebf4c5b156a4df2ab0ce1c569f9f880
Gabriel Galdino <gabrielcgaldiino@gmail.com>
2024-02-28 10:34:17 -0300

    Desenvolvimento da Barra de navegação e Reestruturação para iniciar desenvolvimento responsivo

--- scripts/events.js
+++ scripts/events.js
@@ -1,10 +1,11 @@
+var initialDiv = document.getElementById('me')
+var aboutMe = document.createElement('div')
+
 function showAboutMe() {
-    var initialDiv = document.getElementById('me')
-    var aboutMe = document.createElement('div')
 
     initialDiv.style.display = 'none'
 
-    /**aboutMe.innerHTML = `
+    aboutMe.innerHTML = `
         <div id='aboutme'>
 
         <p> 
@@ -21,27 +22,38 @@ function showAboutMe() {
         </p>
         </div>
     `
-    **/
-    aboutMe.innerHTML = `
-    <div id='aboutme'>
 
-    <p> 
-        To understand a little more about who I have become today, it is necessary to understand where the desire for technology came from. I remember that since I was little,
-        I watched my cousins ​​writing a series of things on the computer screen and most of the time responding with curses because something hadn't worked
-        Sure, I kept paying attention and soon came the shouts of glory for having achieved what they wanted. Later I discovered that they were programmers
-        and that that would be the profession that I would fall in love with in the coming years. It's true that after getting to know the area I discovered what made them have those reactions.
-        when compiling certain code. Still in high school while learning about Programming Logic, I needed to solve chemistry equations,
-        mathematics and physics and was very lazy, so with his own software, which at the time was still based on Portuguese, he developed algorithms that solved
-        problems and give me the answer, at that moment I understood that technology existed to make people's lives easier. A few years later I entered college and began the career I always dreamed of, currently I am looking to join the
-        technology market, my experiences are based on projects developed (available in the projects tab) and work as an implementation analyst
-        and technical support for business management systems (ERP).
-    
-    </p>
-    </div>
-`
    
 
     var sec = document.querySelector('section')
 
     sec.appendChild(aboutMe)
+
+    aboutMe.style.display = 'block'
 }
+
+function showIndex() {
+    aboutMe.style.display = 'none'
+    initialDiv.style.display = 'block'
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var mobileMenu = document.getElementById('navbar');
+    var options = document.getElementById('options');
+    var bars = document.querySelectorAll('.bar');
+    var menu = document.getElementById('menu')
+
+    mobileMenu.addEventListener('click', function () {
+        options.style.display = options.style.display === 'none' ? 'block' : 'none';
+
+        rotateBars();
+        function rotateBars(isX) {
+            var isOpen = options.style.display === 'block';
+            var rotation = isOpen ? '45deg' : '0';
+    
+            bars[0].style.transform = 'rotate(' + rotation + ')';
+            bars[1].style.opacity = isOpen ? '0' : '1';
+            bars[2].style.transform = 'rotate(' + (-rotation) + ')';
+        }
+    });
+});
